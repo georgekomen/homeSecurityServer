bf8ace0dbec555e2ac871147f2afcfd4fa8aa00b
KOMEN <g.komen@carepay.co.ke>
2018-08-24 21:41:54 +0300

    add actuator

---
+++ routes/actuator.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var africastalking = require('../Service/africastalking');
+
+router.post('/', function(req, res, next) {
+    // res.status(200).json(res1);
+});
+
+router.get('/', function(req, res, next) {
+  
+});
+
+module.exports = router;
