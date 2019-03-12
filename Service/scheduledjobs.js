9b642f08115730faf7dbf634e93bab180b56d003
George <g.komen@carepay.co.ke>
2019-03-12 20:00:18 +0300

    use ecm 6

--- Service/scheduledjobs.js
+++ Service/scheduledjobs.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
  
-exports.schedulejob1 = function() {
+exports.schedulejob1 = () => {
     schedule.scheduleJob('00 10 * * *', () => { //sec,min,hour,date,month,day
 
     });
