89310b3d0cc552c22b95d73cb1758ad7c46ac029
George <g.komen@carepay.co.ke>
2019-03-12 19:51:43 +0300

    update fn

--- blink.js
+++ blink.js
@@ -2,9 +2,8 @@ const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 const LED = new Gpio(5, 'out'); //use GPIO pin 5, and specify that it is output
 
 exports.blinkGpio = (req, res) => {
-  let blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
-  function blinkLED() { //function to start blinking
+  const blinkLED = () => { //function to start blinking
     if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
       LED.writeSync(1); //set pin state to 1 (turn LED on)
     } else {
@@ -18,6 +17,8 @@ exports.blinkGpio = (req, res) => {
     res.send('blink ended');
     // LED.unexport(); // Unexport GPIO to free resources
   };
+
+  let blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
   setTimeout(endBlink, 5000); //stop blinking after 5 seconds
 };
