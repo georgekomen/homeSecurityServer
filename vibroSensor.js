12d7b5ebab2f044ccf998b191e39dfb3cc5c8057
komen <g.komen@carepay.co.ke>
2019-03-16 19:31:35 +0300

    only check rising

--- vibroSensor.js
+++ vibroSensor.js
@@ -1,5 +1,5 @@
 const Gpio = require('onoff').Gpio;
-const button = new Gpio(14, 'in', 'both', {debounceTimeout: 40});
+const button = new Gpio(14, 'in', 'rising', {debounceTimeout: 40});
 
 exports.senseVibration = () => {
     button.watch((err, value) => {
