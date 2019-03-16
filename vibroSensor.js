841d1c6445aa23cdf9f71601af3b8255ad799a0c
komen <g.komen@carepay.co.ke>
2019-03-16 19:29:59 +0300

    add debouncing

--- vibroSensor.js
+++ vibroSensor.js
@@ -1,5 +1,5 @@
 const Gpio = require('onoff').Gpio;
-const button = new Gpio(14, 'in', 'both');
+const button = new Gpio(14, 'in', 'both', {debounceTimeout: 40});
 
 exports.senseVibration = () => {
     button.watch((err, value) => {
