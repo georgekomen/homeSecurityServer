69f038f3641888e94fe802b0af43556bc84eb31c
komen <g.komen@carepay.co.ke>
2019-03-17 19:53:23 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -9,8 +9,8 @@ exports.serialcommunication = () => {
         console.log('serial open');
 
         // Because there's no callback to write, write errors will be emitted on the port:
-        port.write('ATE1\r');
-        port.write('AT\r');
+        port.write('ATE1');
+        port.write('AT');
     });
 
     // The open event is always emitted
