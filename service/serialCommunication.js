895f84abeaf57736777ec608e6ee71db2ba77ff7
komen <g.komen@carepay.co.ke>
2019-03-17 20:17:18 +0300

    read serial inside open event

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -1,5 +1,5 @@
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/serial0', { autoOpen: false, baudRate: 115200 });
+const port = new SerialPort('/dev/serial0', { autoOpen: false, baudRate: 9600 });
 
 exports.serialcommunication = () => {
     port.open((err) => {
@@ -8,25 +8,32 @@ exports.serialcommunication = () => {
         }
         console.log('serial open');
 
+        // Read data that is available but keep the stream in "paused mode"
+        port.on('readable', () => {
+            console.log('serial in');
+            console.log('Data:', port.read());
+        });
+
         // Because there's no callback to write, write errors will be emitted on the port:
-        port.write('ATE1');
-        port.write('AT');
+        port.write('ATE1', (err, results) => {
+            console.log('err ' + err);
+            console.log('results ' + results);
+        });
+
+        port.write('AT', (err, results) => {
+            console.log('err ' + err);
+            console.log('results ' + results);
+        });
+
+        // Switches the port into "flowing mode"
+        // port.on('data', (data) => {
+        //     console.log('Data:', data);
+        // });
     });
 
     // The open event is always emitted
     port.on('open', () => {
         // open logic
         console.log('serial open event');
-    });
-
-    // Read data that is available but keep the stream in "paused mode"
-    // port.on('readable', () => {
-    //     console.log('serial in');
-    //     console.log('Data:', port.read());
-    // });
-
-    // Switches the port into "flowing mode"
-    port.on('data', (data) => {
-        console.log('Data:', data);
     });
 }
