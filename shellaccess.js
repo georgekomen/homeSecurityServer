98a4ef140f1e5ab808c411ffd94ef3b994572db2
komen <g.komen@carepay.co.ke>
2019-03-18 23:24:02 +0300

    #

--- shellaccess.js
+++ shellaccess.js
@@ -1,11 +1,15 @@
 const shell = require('shelljs');
 const { spawn } = require('child_process');
+const serialport = require('serialport');
+const port = new serialport('/dev/ttyS0');
 
 exports.accessshell = () => {
     const readserial = () => {
         // commandExe('echo \'AT\' > /dev/ttyS0');
-        const child = spawn('sudo cat /dev/ttyS0');
+        const child = spawn('cat /dev/ttyS0');
         // const child = spawn('pwd');
+
+        console.log(port);
 
         process.stdin.pipe(child.stdin);
 
