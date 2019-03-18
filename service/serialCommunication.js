78569ac246b67801f9263edc61120293cb558967
komen <g.komen@carepay.co.ke>
2019-03-18 15:24:28 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -3,12 +3,12 @@ const default_settings = {baudRate: 9600, autoOpen: false };
 const port = new serialport('/dev/serial0', default_settings);
 const Readline = require('@serialport/parser-readline');
 const parserReadLine =  new Readline();
-// const Delimiter = require('@serialport/parser-delimiter');
-// const parserDelimeter = new Delimiter({ delimiter: '\n' });
+const Delimiter = require('@serialport/parser-delimiter');
+const parserDelimeter = new Delimiter({ delimiter: '\r\n' });
 
 exports.serialcommunication = () => {
-    // port.pipe(parserDelimeter);
-    port.pipe(parserReadLine);
+    port.pipe(parserDelimeter);
+    // port.pipe(parserReadLine);
     port.open((err) => {
         if (err) {
             return console.log('Error opening port: ', err.message);
