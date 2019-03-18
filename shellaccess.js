53b65376364e2c4ea1536cb5bdf8b79f232dc3c7
komen <g.komen@carepay.co.ke>
2019-03-18 21:22:44 +0300

    #

--- shellaccess.js
+++ shellaccess.js
@@ -1,11 +1,23 @@
 const shell = require('shelljs');
+const exec = require('child_process').exec;
+
 
 exports.accessshell = () => {
     const readserial = () => {
-        shell.echo('"AT" > /dev/ttyS0');
-        var str = shell.cat('/dev/ttyS0');
+        shell.echo('\'AT\' > /dev/ttyS0');
+        var str = cat('/dev/ttyS0');
         console.log(str);
+
+        const child = exec('pwd', (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
     }
 
     const readSerial = setInterval(readserial, 250);
 }
+
+
