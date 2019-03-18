a9700961ebabca3d2f6a53a381243f53fa1dea1d
komen <g.komen@carepay.co.ke>
2019-03-18 14:39:47 +0300

    add parser

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -2,16 +2,17 @@ const serialport = require('serialport');
 const default_settings = {baudRate: 9600, autoOpen: false };
 const port = new serialport('/dev/serial0', default_settings);
 const Readline = require('@serialport/parser-readline');
-const parser = new Readline();
+const Delimiter = require('@serialport/parser-delimiter')
+const parserReadLine =  port.pipe(new Readline());
+const parserDelimeter = port.pipe(new Delimiter({ delimiter: '\r\n' }))
 
 exports.serialcommunication = () => {
-    port.pipe(parser);
     port.open((err) => {
         if (err) {
             return console.log('Error opening port: ', err.message);
         }
 
-        parser.on('data', line => {
+        parserDelimeter.on('data', line => {
             console.log(`> ${line}`);
             if(line.includes('RING')){
                 console.log('receiving call');
