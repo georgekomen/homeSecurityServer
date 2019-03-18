d2038144154292e38e6538df22559026cc4bb577
komen <g.komen@carepay.co.ke>
2019-03-18 15:04:59 +0300

    fix potential bug

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -4,8 +4,11 @@ exports.serialcommunication = () => {
     const port = new serialport('/dev/serial0', default_settings);
     const Readline = require('@serialport/parser-readline');
     const Delimiter = require('@serialport/parser-delimiter');
-    const parserReadLine =  port.pipe(new Readline());
-    // const parserDelimeter = port.pipe(new Delimiter({ delimiter: '\n' }));
+    const parserReadLine =  new Readline();
+    port.pipe(parserReadLine);
+    const parserDelimeter = new Delimiter({ delimiter: '\n' });
+    // port.pipe(parserDelimeter);
+
     port.open((err) => {
         if (err) {
             return console.log('Error opening port: ', err.message);
