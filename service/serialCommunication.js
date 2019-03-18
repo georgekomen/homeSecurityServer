e4a150f1f5b0ef8f68515a5a6462a752240cd7c1
komen <g.komen@carepay.co.ke>
2019-03-18 14:48:39 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -1,12 +1,13 @@
-const serialport = require('serialport');
-const default_settings = {baudRate: 9600, autoOpen: false };
-const port = new serialport('/dev/serial0', default_settings);
-const Readline = require('@serialport/parser-readline');
-const Delimiter = require('@serialport/parser-delimiter')
-const parserReadLine =  port.pipe(new Readline());
-const parserDelimeter = port.pipe(new Delimiter({ delimiter: '\r\n' }))
-
 exports.serialcommunication = () => {
+    const serialport = require('serialport');
+    const default_settings = {baudRate: 9600, autoOpen: false };
+    const port = new serialport('/dev/serial0', default_settings);
+    const Readline = require('@serialport/parser-readline');
+    const Delimiter = require('@serialport/parser-delimiter')
+    const parserReadLine =  port.pipe(new Readline());
+    const parserDelimeter = port.pipe(new Delimiter({ delimiter: '\r\n' }))
+
+
     port.open((err) => {
         if (err) {
             return console.log('Error opening port: ', err.message);
