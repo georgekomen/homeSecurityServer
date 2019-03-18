8ebb088f3fb8e1fc06ca345da520d74ab6c3cedb
komen <g.komen@carepay.co.ke>
2019-03-18 08:04:12 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -1,5 +1,5 @@
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/serial0', { autoOpen: false, baudRate: 9600 });
+const port = new SerialPort('/dev/serial0', { baudRate: 9600, parser: SerialPort.parsers.readline("\n")}, false);
 
 exports.serialcommunication = () => {
     port.open((err) => {
@@ -7,18 +7,22 @@ exports.serialcommunication = () => {
             return console.log('Error opening port: ', err.message);
         }
         console.log('serial open');
+
+        port.write('AT\r', (err, results) => {
+            console.log(err, results);
+        });
     });
 
     // Switches the port into "flowing mode"
-    // port.on('data', (data) => {
-    //     console.log('Data:', data);
-    // });
+    port.on('data', (data) => {
+        console.log('Data:', data);
+    });
 
     // Read data that is available but keep the stream in "paused mode"
-    port.on('readable', () => {
-        console.log('serial in');
-        console.log('Data:', port.read());
-    });
+    // port.on('readable', () => {
+    //     console.log('serial in');
+    //     console.log('Data:', port.read());
+    // });
 
     port.on('error', (err) => {
         console.log('node-serialport error!!!');
