865b67c4811d8ac4da398c8d7ce49a762e51372b
komen <g.komen@carepay.co.ke>
2019-03-18 22:05:19 +0300

    use spwan child process

--- shellaccess.js
+++ shellaccess.js
@@ -1,24 +1,17 @@
 const shell = require('shelljs');
-const exec = require('child_process').exec;
+const { spawn } = require('child_process');
 
 exports.accessshell = () => {
     const readserial = () => {
         // commandExe('echo \'AT\' > /dev/ttyS0');
-        commandExe('cat /dev/ttyS0');
-    }
+        const child = spawn('cat /dev/ttyS0');
 
-    const commandExe = (command) => {
-        console.log(command);
-        exec(command, (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            if (error) {
-                console.log(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-            }
+        child.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`);
         });
     }
+
+
     const readSerial = setInterval(readserial, 100);
 }
 
