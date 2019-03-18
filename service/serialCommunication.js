4f861b9ad9a79436e816b297e3ca41fad573ce3c
komen <g.komen@carepay.co.ke>
2019-03-18 11:16:46 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -17,11 +17,11 @@ exports.serialcommunication = () => {
             console.log('serial error!');
         });
 
-        port.write(new Buffer.from("AT"), (err, res) => {
+        port.write(new Buffer.from("AT\n"), (err, res) => {
             console.log(err, res);
         });
 
-        parser.write(new Buffer.from("AT"), (err, res) => {
+        parser.write(new Buffer.from("AT\n"), (err, res) => {
             console.log(err, res);
         });
 
