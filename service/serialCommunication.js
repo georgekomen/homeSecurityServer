f2fe7d6e4866a0c8ef7c8af9a94bb83801b3c4e0
komen <g.komen@carepay.co.ke>
2019-03-18 10:24:28 +0300

    add parser

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -1,12 +1,17 @@
 const serialport = require('serialport');
 const default_settings = {baudRate: 9600, autoOpen: false };
 const port = new serialport('/dev/serial0', default_settings);
+const Readline = require('@serialport/parser-readline');
+const parser = new Readline();
 
 exports.serialcommunication = () => {
+    port.pipe(parser);
     port.open((err) => {
         if (err) {
             return console.log('Error opening port: ', err.message);
         }
+
+        parser.on('data', line => console.log(`> ${line}`));
 
         // Switches the port into "flowing mode"
         port.on('data', (data) => {
