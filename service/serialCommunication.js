fdc406cbf71c70f9b69bd184a2e222d0fc4e365a
komen <g.komen@carepay.co.ke>
2019-03-19 23:39:49 +0300

    #

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -1,5 +1,5 @@
 const serialport = require('serialport');
-const default_settings = {baudRate: 9600, autoOpen: true, lock: false };
+const default_settings = {baudRate: 9600, autoOpen: false, lock: false };
 const port = new serialport('/dev/ttyS0', default_settings);
 const Readline = require('@serialport/parser-readline');
 const parserReadLine =  new Readline({ delimiter: '\r\n' });
@@ -10,18 +10,31 @@ exports.serialcommunication = () => {
     // port.pipe(parserDelimeter);
     port.pipe(parserReadLine);
 
-    port.write('AT\n');
-
-    parserReadLine.on('data', data => {
-        console.log(`> ${data}`);
-        if(data.includes('RING')) {
-            console.log('receiving call');
-            port.write('ATA\r');
+    port.open( (err) => {
+        if (err) {
+            return console.log('Error opening port: ', err.message)
         }
-    });
 
-    // Open errors will be emitted as an error event
-    port.on('error', (err) => {
-        console.log('Error: ', err.message);
+        port.set({dtr: true, rts: false}, (err) => {
+            if(err) {
+                console.log('Set Error: ', err.message);
+            }
+            console.log('Set Done');
+        });
+
+        port.write('AT\n');
+
+        parserReadLine.on('data', data => {
+            console.log(`> ${data}`);
+            if(data.includes('RING')) {
+                console.log('receiving call');
+                port.write('ATA\r');
+            }
+        });
+
+        // Open errors will be emitted as an error event
+        port.on('error', (err) => {
+            console.log('Error: ', err.message);
+        });
     });
 };
