f1a7b9acffa2db852509bc5931b126ac0e14989c
komen <g.komen@carepay.co.ke>
2019-03-20 17:28:24 +0300

    get gps location

--- service/serialCommunication.js
+++ service/serialCommunication.js
@@ -28,6 +28,7 @@ exports.serialcommunication = () => {
             console.log('receiving call');
             port.write('AT+DDET=1\r\n'); //enable dtmf
             port.write('ATA\r\n'); //receive call
+            port.write('AT+CIPGSMLOC=1,1\r\n'); //get gps loc
         }
         if(data.includes('DTMF')) {
             const code = data[data.length -1];
