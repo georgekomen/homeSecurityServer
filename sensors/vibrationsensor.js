eafe13ff8caef75a254e2340d56410d79e52cd88
komen <g.komen@carepay.co.ke>
2019-03-21 01:20:07 +0300

    dont use pull up

--- sensors/vibrationsensor.js
+++ sensors/vibrationsensor.js
@@ -4,24 +4,38 @@ const board = new five.Board({
                                  io: new Raspi()
                              });
 exports.senseVibration = () => {
-    let button, led;
-    board.on("ready", () => {
-        console.log('sensor ready');
-        button = new five.Button({
-                                     pin: 'GPIO6',
-                                     isPullup: true
-                                 });
+    let button;
+    board.on("ready", function() {
 
-        led = new five.Led('GPIO12');
+        // Create a new `button` hardware instance.
+        // This example allows the button module to
+        // create a completely default instance
+        button = new five.Button(2);
 
-        button.on("down", (value) => {
-            console.log(value);
-            led.on();
+        // Inject the `button` hardware into
+        // the Repl instance's context;
+        // allows direct command line access
+        board.repl.inject({
+                              button: button
+                          });
+
+        // Button Event API
+
+        // "down" the button is pressed
+        button.on("down", function() {
+            console.log("down");
         });
 
-        button.on("up", (value) => {
-            console.log(value);
-            led.off();
+        // "hold" the button is pressed for specified time.
+        //        defaults to 500ms (1/2 second)
+        //        set
+        button.on("hold", function() {
+            console.log("hold");
+        });
+
+        // "up" the button is released
+        button.on("up", function() {
+            console.log("up");
         });
     });
 };
