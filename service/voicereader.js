7a0076c1a6ac870b218322526de3415a5095b2a1
komen <g.komen@carepay.co.ke>
2019-03-21 22:51:24 +0300

    try read buffer from float32array

--- service/voicereader.js
+++ service/voicereader.js
@@ -20,7 +20,7 @@ exports.readaudio = (serialport) => {
         serialport.write('AT+VSM=128,8000\r');//Compression Method: 8-bit linear / Sampling Rate: 8000MHz
         serialport.write('AT+VLS=1\r');//put modem into TAD mode
         serialport.write('AT+VTX\r');//put modem into TAD mode
-        serialport.write(Buffer.from(file.buffer, 0, 16));
+        serialport.write(file[0]);
         // serialport.write(Buffer.from(file.buffer));
     };
 
